38305d958c0c886963e23ebde3a71e0f75983b0a
Luis <142854556+AstroLui@users.noreply.github.com>
2023-09-08 20:27:40 -0400

    flet: add methods for multiple sorting

--- sortingmethods.py
+++ sortingmethods.py
@@ -3,15 +3,15 @@
 # 
 #1. MergeSort: Ordenamiento de costo total por un rango de Fechas
 #  
-def mergesortRangoASC(arr): 
+def mergesort_RangoFechas_ASC(arr): 
     if len(arr) > 1: 
         mid = len(arr)//2
 
         L = arr[:mid]
         R = arr[mid:]
 
-        mergesortRangoASC(L)
-        mergesortRangoASC(R)
+        mergesort_RangoFechas_ASC(L)
+        mergesort_RangoFechas_ASC(R)
 
         i = j = k = 0
 
@@ -34,15 +34,15 @@ def mergesortRangoASC(arr):
             j += 1
             k += 1
 
-def mergesortRangoDESC(arr): 
+def mergesort_RangoFechas_DESC(arr): 
     if len(arr) > 1: 
         mid = len(arr)//2
 
         L = arr[:mid]
         R = arr[mid:]
 
-        mergesortRangoDESC(L,)
-        mergesortRangoDESC(R,)
+        mergesort_RangoFechas_DESC(L,)
+        mergesort_RangoFechas_DESC(R,)
 
         i = j = k = 0
 
@@ -66,43 +66,96 @@ def mergesortRangoDESC(arr):
             k += 1
 
 #
-# TODAVIA FALTA MODIFICAR
+#
 #2. Quicksort: Ordenamiento Multiple
 #
-def quickSort(arr, low, high):
-    if len(arr) == 1:
-        return arr
+def quickSort_NoMultiple_ASC(arr, low, high):
     if low < high:
-        pi = __particion(arr, low, high)
-        quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high)
+        pi = __particion_NoMultiple_ASC(arr, low, high)
+        quickSort_NoMultiple_ASC(arr, low, pi-1)
+        quickSort_NoMultiple_ASC(arr, pi+1, high)
     return arr
 
+def __particion_NoMultiple_ASC(arr, low, high):
+    pivot = arr[high] 
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <=  pivot:
+            i +=1
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    i = i + 1
+    (arr[i], arr[high]) = (arr[high], arr[i])
+    return i
 
-def __particion(arr, low, high):
-    pivot = arr[high]
+def quickSort_NoMultiple_DESC(arr, low, high):
+    if low < high:
+        pi = __particion_NoMultiple_DESC(arr, low, high)
+        quickSort_NoMultiple_DESC(arr, low, pi-1)
+        quickSort_NoMultiple_DESC(arr, pi+1, high)
+    return arr
+
+def __particion_NoMultiple_DESC(arr, low, high):
+    pivot = arr[high] 
     i = low - 1
     for j in range(low, high):
         if arr[j] >=  pivot:
             i +=1
             (arr[i], arr[j]) = (arr[j], arr[i])
     i = i + 1
-    (arr[i], arr[high]) = (arr[high], arr[i])  
+    (arr[i], arr[high]) = (arr[high], arr[i])
     return i
+
+def quickSort_Multiple_ASC(arr):
+    if len(arr) <= 1:
+        return arr
+    smaller, larger, pi= __particion_Multiple_ASC(arr)
+    return quickSort_Multiple_ASC(smaller) + [pi] + quickSort_Multiple_ASC(larger)
+
+def __particion_Multiple_ASC(arr):
+    pivot = arr[len(arr) // 2]
+    smaller = []
+    larger = []
+    for item in arr:
+        if item.getDuracion() < pivot.getDuracion() and item.getCostoTotal() < pivot.getCostoTotal():
+            smaller.append(item)
+
+        elif item.getDuracion() > pivot.getDuracion() and item.getCostoTotal() > pivot.getCostoTotal():
+            larger.append(item)
+
+    return smaller, larger, pivot
+
+def quickSort_Multiple_DESC(arr):
+    if len(arr) <= 1:
+        return arr
+    smaller, larger, pi= __particion_Multiple_DESC(arr)
+    return quickSort_Multiple_DESC(smaller) + [pi] + quickSort_Multiple_DESC(larger)
+
+def __particion_Multiple_DESC(arr):
+    pivot = arr[len(arr) // 2]
+    smaller = []
+    larger = []
+    for item in arr:
+        if item.getDuracion() > pivot.getDuracion() and item.getCostoTotal() > pivot.getCostoTotal():
+            smaller.append(item)
+
+        elif item.getDuracion() < pivot.getDuracion() and item.getCostoTotal() < pivot.getCostoTotal():
+            larger.append(item)
+
+    return smaller, larger, pivot
 #
 #3. Heapsort: Ordenamiento segun su duracion de reserva
 #
-def heapSortDuracionASC(arr):
+def heapSort_Duracion_ASC(arr):
     n = len(arr)
 
     for i in range(n//2-1, -1, -1):
-        __heapifyASC(arr, n, i)
+        __heapify_Duracion_ASC(arr, n, i)
     
     for i in range(n-1, 0, -1):
         (arr[i], arr[0]) = (arr[0], arr[i])
-        __heapifyASC(arr, i, 0)
+        __heapify_Duracion_ASC(arr, i, 0)
 
-def __heapifyASC(arr, n, i):
+def __heapify_Duracion_ASC(arr, n, i):
     largerst = i
     l = 2 * i + 1
     r = 2 * i + 2
@@ -116,19 +169,19 @@ def __heapifyASC(arr, n, i):
     if largerst != i:
         (arr[i], arr[largerst]) = (arr[largerst], arr[i])
     
-        __heapifyASC(arr, n, largerst)
+        __heapify_Duracion_ASC(arr, n, largerst)
 
-def heapSortDuracionDESC(arr):
+def heapSort_Duracion_DESC(arr):
     n = len(arr)
 
     for i in range(n//2-1, -1, -1):
-        __heapify(arr, n, i)
+        __heapify_Duracion_DESC(arr, n, i)
     
     for i in range(n-1, 0, -1):
         (arr[i], arr[0]) = (arr[0], arr[i])
-        __heapify(arr, i, 0)
+        __heapify_Duracion_DESC(arr, i, 0)
 
-def __heapify(arr, n, i):
+def __heapify_Duracion_DESC(arr, n, i):
     largerst = i
     l = 2 * i + 1
     r = 2 * i + 2
@@ -142,14 +195,14 @@ def __heapify(arr, n, i):
     if largerst != i:
         (arr[i], arr[largerst]) = (arr[largerst], arr[i])
     
-        __heapify(arr, n, largerst)
+        __heapify_Duracion_DESC(arr, n, largerst)
 
 #
 # NECESITA EL OBJETO "USUARIO" PARA TERMINAR DE MODIFICAR
 #4. Shellsort: Ordenamiento del cliente segun el numero de reservaciones que tenga
 #
 
-def shellsortNoReservacionesDESC(arr):
+def shellsort_NoReservaciones_DESC(arr):
     n = len(arr) 
     gap = n // 2
     while gap > 0: 
@@ -162,7 +215,7 @@ def shellsortNoReservacionesDESC(arr):
             arr[j] = temp 
         gap //= 2
 
-def shellsortNoReservacionesASC(arr):
+def shellsort_NoReservaciones_ASC(arr):
     n = len(arr) 
     gap = n // 2
     while gap > 0: 
