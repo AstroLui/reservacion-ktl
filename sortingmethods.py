aa756cf9eb75b9f140d7270f3bf6d788610217b5
Luis <142854556+AstroLui@users.noreply.github.com>
2023-09-10 22:28:00 -0400

    docs: explanation of methods

--- sortingmethods.py
+++ sortingmethods.py
@@ -1,20 +1,41 @@
-
-#Tipo de Ordenamiento:
 # 
 #1. MergeSort: Ordenamiento de costo total por un rango de Fechas
 #  
 def mergesort_RangoFechas_ASC(arr): 
+    """
+        La funcion `mergesort_RangoFechas_ASC` ordena de forma ascendente una lista tomando
+        como criterio de ordenamiento el costo total de la reservacion
+
+        :param arr: El parametro `arr` es la lista que se desea ordenar, y que contiene todas
+        las reservaciones que cumplan con un rango de fechas
+        :return: Devuelve la lista ya ordenada
+    """
+
+    #Condicion de parada para la recurision que chequea si el tamaño
+    #de la lista es mayor a 1, en caso contrario finalizara la funcion 
+    #retornando la lista
     if len(arr) > 1: 
+
+        #La variable `mid` se usara para ir dividendo la lista `arr` por la mitad
         mid = len(arr)//2
 
-        L = arr[:mid]
-        R = arr[mid:]
+        #La listas `L` y `R` almacenaran los elemento de la lista `arr` que ayudaran
+        # a organizar la lista `arr`
+        L = arr[:mid]   # `L` almacenara los elementos del lado izquierdo hasta la mitad de la lista `arr`
+        R = arr[mid:]   # `R` almacenara los elementos de la mitad hasta el lado derecho de la lista `arr`
 
+        #Llamada recursiva que dividira `L`
         mergesort_RangoFechas_ASC(L)
+
+        #Llamada recursiva que dividira `R`
         mergesort_RangoFechas_ASC(R)
 
+        #Inicializacion de los indices que se usaran para las listas `arr`, `L` y `R`
         i = j = k = 0
 
+        #Ciclo que comparar quien es el menor de los elementos que contiene las lista `L` y `R`
+        # y los almacenara nuevamente en la lista `arr`, este ciclo finalizara cuando cualquiera 
+        # de las dos listas `L` y `R` termine de recorreser por complento 
         while i < len(L) and j < len(R):
             if L[i].getCostoTotal() <= R[j].getCostoTotal():
                 arr[k] = L[i]
@@ -24,28 +45,59 @@ def mergesort_RangoFechas_ASC(arr):
                 j += 1
             k += 1
 
+        #En el caso de que la lista `R` fue el primero en recorrese por completo, este ciclo
+        # almacenara en la lista `arr` los elementos restantes de la lista `L`, y finalizara cuando 
+        # `L` se recorra por completo
         while i < len(L):
             arr[k] = L[i]
             i += 1
             k += 1
-
+    
+        #En el caso de que la lista `L` fue el primero en recorrese por completo, este ciclo
+        # almacena en la lista `arr` los elementos restantes de la lista `R`, y finalizara cuando
+        # `R` se recorra por completo
         while j < len(R):
             arr[k] = R[j]
             j += 1
             k += 1
 
-def mergesort_RangoFechas_DESC(arr): 
+    return arr
+
+def mergesort_RangoFechas_DESC(arr):
+    """
+        La funcion `mergesort_RangoFechas_DESC` ordena de forma descendente una lista tomando
+        como criterio de ordenamiento el costo total de la reservacion
+
+        :param arr: El parametro `arr` es la lista que se desea ordenar, y que contiene todas
+        las reservaciones que cumplan con un rango de fechas
+        :return: Devuelve la lista ya ordenada
+    """
+
+    #Condicion de parada para la recurision que chequea si el tamaño
+    #de la lista es mayor a 1, en caso contrario finalizara la funcion 
+    #retornando la lista
     if len(arr) > 1: 
+        
+        #La variable `mid` se usara para ir dividendo la lista `arr` por la mitad
         mid = len(arr)//2
 
-        L = arr[:mid]
-        R = arr[mid:]
+        #La listas `L` y `R` almacenaran los elemento de la lista `arr` que ayudaran
+        # a organizar la lista `arr`
+        L = arr[:mid]   # `L` almacenara los elementos del lado izquierdo hasta la mitad de la lista `arr`
+        R = arr[mid:]   # `R` almacenara los elementos de la mitad hasta el lado derecho de la lista `arr`
 
-        mergesort_RangoFechas_DESC(L,)
-        mergesort_RangoFechas_DESC(R,)
+        #Llamada recursiva que dividira `L`
+        mergesort_RangoFechas_DESC(L)
+        
+        #Llamada recursiva que dividira `R`
+        mergesort_RangoFechas_DESC(R)
 
+        #Inicializacion de los indices que se usaran para las listas `arr`, `L` y `R`
         i = j = k = 0
 
+        #Ciclo que comparar quien es el menor de los elementos que contiene las lista `L` y `R`
+        # y los almacenara nuevamente en la lista `arr`, este ciclo finalizara cuando cualquiera 
+        # de las dos listas `L` y `R` termine de recorreser por complento 
         while i < len(L) and j < len(R):
             if L[i].getCostoTotal() >= R[j].getCostoTotal():
                 arr[k] = L[i]
@@ -55,17 +107,23 @@ def mergesort_RangoFechas_DESC(arr):
                 j += 1
             k += 1
 
+        #En el caso de que la lista `R` fue el primero en recorrese por completo, este ciclo
+        # almacenara en la lista `arr` los elementos restantes de la lista `L`, y finalizara cuando 
+        # `L` se recorra por completo
         while i < len(L):
             arr[k] = L[i]
             i += 1
             k += 1
 
+        #En el caso de que la lista `L` fue el primero en recorrese por completo, este ciclo
+        # almacena en la lista `arr` los elementos restantes de la lista `R`, y finalizara cuando
+        # `R` se recorra por completo
         while j < len(R):
             arr[k] = R[j]
             j += 1
             k += 1
+    return arr
 
-#
 #
 #2. Quicksort: Ordenamiento Multiple
 #
@@ -146,52 +204,103 @@ def __particion_Multiple_DESC(arr):
 #3. Heapsort: Ordenamiento segun su duracion de reserva
 #
 def heapSort_Duracion_ASC(arr):
+    """
+        La funcion `heapSort_Duracion_ASC` ordena de forma ascendente una lista tomando
+        como criterio de ordenamiento la duracion de la reservar
+
+        :param arr: El parametro `arr` representa la lista que se desea ordenar
+    """
+    # Inicializamos `n` almacenando la longitud de la lista
     n = len(arr)
 
+    #Ciclo que construye el arbol binario y lo ordena 
+    # de mayor a menor
     for i in range(n//2-1, -1, -1):
         __heapify_Duracion_ASC(arr, n, i)
     
+    # Ciclo que extrae los elementos uno a uno y los 
+    # almacena nuevamente en `arr` de forma ordenada
     for i in range(n-1, 0, -1):
         (arr[i], arr[0]) = (arr[0], arr[i])
         __heapify_Duracion_ASC(arr, i, 0)
 
 def __heapify_Duracion_ASC(arr, n, i):
-    largerst = i
-    l = 2 * i + 1
-    r = 2 * i + 2
+    """
+    La función `__heapify_Duracion_ASC` compara los elementos de la lista de para ordenarlos de forma
+    ascendente
+    
+    :param arr: el parametro `arr` es una lista que contiene elementos que es necesario reorganizarlo
+    :param n: el parametro "n" representa el tamaño de la lista `arr` o el número de elementos en la lista `arr`
+    :param i: el parametro "i" representa el índice del nodo actual
+    """
 
+    #Inicializamos los indices de los nodos
+    largerst = i   # Indice para el nodo principal
+    l = 2 * i + 1   # Indice de los nodos izquierdas 
+    r = 2 * i + 2   # Indice de las nodos derechas
+
+    # Condicion que compara si son mayores los nodos secundario 
+    # de la izquierda con el nodo principal
     if l < n and arr[i].getDuracion() < arr[l].getDuracion():
         largerst = l
     
+    # Condicion que compara si son mayores los nodos secundario 
+    # de la derecha con el nodo principal
     if r < n and arr[largerst].getDuracion() < arr[r].getDuracion():
         largerst = r
 
+    # Condicion que cambia la variable `largerst` si es necesario
     if largerst != i:
         (arr[i], arr[largerst]) = (arr[largerst], arr[i])
-    
+        
         __heapify_Duracion_ASC(arr, n, largerst)
 
 def heapSort_Duracion_DESC(arr):
+    """
+        La funcion `heapSort_Duracion_DESC` ordena de forma descendente una lista tomando
+        como criterio de ordenamiento la duracion de la reservar
+        
+        :param arr: El parametro `arr` representa la lista que se desea ordenar
+    """
+    # Inicializamos `n` almacenando la longitud de la lista
     n = len(arr)
 
+    #Ciclo que construye el arbol binario y lo ordena 
+    # de menor a mayor
     for i in range(n//2-1, -1, -1):
         __heapify_Duracion_DESC(arr, n, i)
     
+    # Ciclo que extrae los elementos uno a uno y los 
+    # almacena nuevamente en `arr` de forma ordenada
     for i in range(n-1, 0, -1):
         (arr[i], arr[0]) = (arr[0], arr[i])
         __heapify_Duracion_DESC(arr, i, 0)
 
 def __heapify_Duracion_DESC(arr, n, i):
-    largerst = i
-    l = 2 * i + 1
-    r = 2 * i + 2
+    """
+    La función `__heapify_Duracion_DESC` compara los elementos de la lista de para ordenarlos de forma
+    descendente
+    
+    :param arr: el parametro `arr` es una lista que contiene elementos que es necesario reorganizarlo
+    :param n: el parametro "n" representa el tamaño de la lista `arr` o el número de elementos en la lista `arr`
+    :param i: el parametro "i" representa el índice del nodo actual
+    """
+    #Inicializamos los indices de los nodos
+    largerst = i   # Indice para el nodo principal
+    l = 2 * i + 1   # Indice de los nodos izquierdas 
+    r = 2 * i + 2   # Indice de las nodos derechas
 
+     # Condicion que compara si son menores los nodos secundario 
+    # de la izquierda con el nodo principal
     if l < n and arr[i].getDuracion() > arr[l].getDuracion():
         largerst = l
     
+    # Condicion que compara si son menores los nodos secundario 
+    # de la derecha con el nodo principal
     if r < n and arr[largerst].getDuracion() > arr[r].getDuracion():
         largerst = r
 
+     # Condicion que cambia la variable `largerst` si es necesario
     if largerst != i:
         (arr[i], arr[largerst]) = (arr[largerst], arr[i])
     
@@ -201,27 +310,68 @@ def __heapify_Duracion_DESC(arr, n, i):
 #4. Shellsort: Ordenamiento del cliente segun el numero de reservaciones que tenga
 #
 def shellsort_NoReservaciones_DESC(arr):
-    n = len(arr) 
-    gap = n // 2
+    """
+        La funcion `shellsort_NoReservaciones_DESC` ordena de forma descendiente una lista
+        tomando como criterio el numero de reservaciones 
+
+        :param arr: El parametro `arr` representa la lista que se quiere ordenar
+    """
+    
+    n = len(arr)    #Almacenamos el tamaño de la lista
+    
+    gap = n // 2    #Inicializamos el tamaño de espacio 
+
+    # Realiza una clasificación de inserción con espacios usando la variable `gap`.
+    # Los primeros elementos del espacio a[0..gap-1] ya están en el espacio
+    # orden sigue añadiendo un elemento más hasta completar el array
+    # está ordenado por espacios
     while gap > 0: 
         for i in range(gap, n): 
+
+            # Agregamos arr[i] a los elementos que han sido ordenados por espacios
+            # guardamos arr[i] en una variable temporal `temp` y hacemos un agujero en la posición i
             temp = arr[i]
+            
+            # Desplazar elementos anteriores ordenados por espacios hasta encontrar 
+            # la ubicación correcta para arr[i]
             j = i
             while  j >= gap and arr[j - gap].getTotalReservaciones() < temp.getTotalReservaciones(): 
                 arr[j] = arr[j - gap]
                 j -= gap  
+
+            # Colcamos temp en la arr[i] en su ubicación correcta
             arr[j] = temp 
         gap //= 2
 
 def shellsort_NoReservaciones_ASC(arr):
-    n = len(arr) 
-    gap = n // 2
+    """
+        La funcion `shellsort_NoReservaciones_DESC` ordena de forma descendiente una lista
+        tomando como criterio el numero de reservaciones 
+
+        :param arr: El parametro `arr` representa la lista que se quiere ordenar
+    """
+    n = len(arr)    #Almacenamos el tamaño de la lista
+    
+    gap = n // 2    #Inicializamos el tamaño de espacio 
+
+    # Realiza una clasificación de inserción con espacios usando la variable `gap`.
+    # Los primeros elementos del espacio a[0..gap-1] ya están en el espacio
+    # orden sigue añadiendo un elemento más hasta completar el array
+    # está ordenado por espacios
     while gap > 0: 
-        for i in range(gap, n): 
+        for i in range(gap, n):
+
+            # Agregamos arr[i] a los elementos que han sido ordenados por espacios
+            # guardamos arr[i] en una variable temporal `temp` y hacemos un agujero en la posición i
             temp = arr[i]
+
+            # Desplazar elementos anteriores ordenados por espacios hasta encontrar 
+            # la ubicación correcta para arr[i]
             j = i
             while  j >= gap and arr[j - gap].getTotalReservaciones() > temp.getTotalReservaciones(): 
                 arr[j] = arr[j - gap]
                 j -= gap  
+                
+            #Colcamos temp en la arr[i] en su ubicación correcta
             arr[j] = temp 
         gap //= 2
